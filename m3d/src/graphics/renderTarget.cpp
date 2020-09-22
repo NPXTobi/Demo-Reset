10fb69c83936c8f422e741c9a1eccf6bc2b61e2a
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-22 15:37:36 +0200

    Add files via upload

---
+++ m3d/src/graphics/renderTarget.cpp
@@ -0,0 +1,35 @@
+#include "m3d/graphics/renderTarget.hpp"
+#include "m3d/graphics/color.hpp"
+
+namespace m3d {
+    RenderTarget::RenderTarget(int t_width, int t_height) :
+        m_width(t_width),
+        m_height(t_height),
+        m_clearColor(m3d::Color::rgba8(0, 0, 0, 255)) {
+            m_target = C3D_RenderTargetCreate(t_height, t_width, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+    }
+
+    C3D_RenderTarget* RenderTarget::getRenderTarget() {
+        return m_target;
+    }
+
+    int RenderTarget::getWidth() {
+        return m_width;
+    }
+
+    int RenderTarget::getHeight() {
+        return m_height;
+    }
+
+    void RenderTarget::setClearColor(u32 t_color) {
+        m_clearColor = t_color;
+    }
+
+    void RenderTarget::clear() {
+        C2D_TargetClear(m_target, m_clearColor);
+    }
+
+    u32 RenderTarget::getClearColor() {
+        return m_clearColor;
+    }
+} /* m3d */
