31f45b2ec13fa246fc6f9ad091b30c1a48eb057c
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-22 15:46:35 +0200

    Add files via upload

---
+++ m3d/inc/m3d/input/input.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file input.hpp
+ * @brief Includes all files related to user input
+ */
+#ifndef INPUT_H
+#define INPUT_H
+
+#pragma once
+
+#include "buttons.hpp"
+#include "circlepad.hpp"
+#include "softwareKeyboard.hpp"
+#include "touch.hpp"
+
+
+#endif /* end of include guard: INPUT_H */
