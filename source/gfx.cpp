53fc18a2a9e4f616a8cb3e0b5dccba954c752470
Tobi <71647024+Tobi-D7@users.noreply.github.com>
2021-01-07 00:17:51 +0100

    Update gfx.cpp

--- source/gfx.cpp
+++ source/gfx.cpp
@@ -1,8 +1,12 @@
 #include "gfx.hpp"
 #include "common.hpp"
+#include "app.hpp"
+
+extern C2D_SpriteSheet sprites;
 void GFX::DrawTop(){
     Gui::ScreenDraw(Top);
-    Gui::Draw_Rect(0, 0, 400, 240, C2D_Color32(0, 100, 255, 255));
+    Gui::DrawSprite(sprites, 1, 0, 0, 1.0f, 1.0f);
+    //Gui::Draw_Rect(0, 0, 400, 240, C2D_Color32(0, 100, 255, 255));
 }
 void GFX::DrawBottom(){
     Gui::ScreenDraw(Bottom);
