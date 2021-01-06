4d4b237ab4b672a610aa519241963f6eb5b155bc
Tobi <71647024+Tobi-D7@users.noreply.github.com>
2021-01-06 23:44:21 +0100

    Update resetm.cpp

--- source/resetm.cpp
+++ source/resetm.cpp
@@ -5,6 +5,7 @@ extern bool exiting;
 
 void ResetMenu::Draw(void) const {
     GFX::DrawTop();
+	Gui::DrawString(10, 01, 1.0f, C2D_Color32(255, 255, 255, 255), "App Works!");
 }
 void ResetMenu::Logic(u32 hDown, u32 hHeld, touchPosition touch) {
 
