7750ef12d2f27142742d48f76d1a11ecc4d0148e
Tobi <71647024+Tobi-D7@users.noreply.github.com>
2021-01-11 10:52:10 +0000

    Update gfx.cpp

--- source/gfx.cpp
+++ source/gfx.cpp
@@ -5,10 +5,10 @@
 extern C2D_SpriteSheet sprites;
 void GFX::DrawTop(){
     Gui::ScreenDraw(Top);
-    Gui::DrawSprite(sprites, 1, 0, 0, 1.0f, 1.0f);
+    //Gui::DrawSprite(sprites, 1, 0, 0, 1.0f, 1.0f);
 }
 void GFX::DrawBottom(){
     Gui::ScreenDraw(Bottom);
-    Gui::DrawSprite(sprites, 2, 0, 0, 1.0f, 1.0f);
-    Gui::DrawSprite(sprites, 3, 0, 0, 0.03f, 0.03f);
+    //Gui::DrawSprite(sprites, 2, 0, 0, 1.0f, 1.0f);
+    //Gui::DrawSprite(sprites, 3, 0, 0, 0.03f, 0.03f);
 }
